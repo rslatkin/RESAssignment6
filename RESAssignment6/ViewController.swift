4c5fe4df56d4406338d4d01d2f5d249f2fee5895
Robert Slatkin <robertslatkin@Roberts-MacBook-Pro-2.local>
2015-04-08 23:25:06 -0400

    Initial Commit

---
+++ RESAssignment6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RESAssignment6
+//
+//  Created by Robert Slatkin on 4/8/15.
+//  Copyright (c) 2015 Robert Slatkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
